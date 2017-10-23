b1e51b98ec34192398de114bb3a6e93c573b4ae9
chenyifaer <1157038440@qq.com>
2017-10-23 11:39:23 +0800

    通知界面 简单修改 checkbox5

--- app/src/main/java/a1506a4/bwie/com/bwapp/app/MyApplication.java
+++ app/src/main/java/a1506a4/bwie/com/bwapp/app/MyApplication.java
@@ -3,11 +3,6 @@ package a1506a4.bwie.com.bwapp.app;
 import android.app.Application;
 import android.content.Context;
 
-import com.wxn.locationutil.LocationUtil;
-import com.wxn.locationutil.PermissionUtil;
-
-import a1506a4.bwie.com.bwapp.view.activity.MainActivity;
-
 /**
  * Created by Shadow on 2017/10/13.
  */
